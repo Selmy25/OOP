cc150d51daf643242a028ccda7975fe54ae1483b
Selmy25 <emil.salmanov25@mail.ru>
2023-05-25 00:06:41 +0500

    First commit

---
+++ laba4_2/laba4_2/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace laba4_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.numericUpDownA = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownB = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownC = new System.Windows.Forms.NumericUpDown();
+            this.trackBarA = new System.Windows.Forms.TrackBar();
+            this.trackBarB = new System.Windows.Forms.TrackBar();
+            this.trackBarC = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarC)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(173, 190);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(120, 22);
+            this.textBoxA.TabIndex = 0;
+            this.textBoxA.Leave += new System.EventHandler(this.textBoxA_Leave);
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(340, 190);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(120, 22);
+            this.textBoxB.TabIndex = 1;
+            this.textBoxB.Leave += new System.EventHandler(this.textBoxB_Leave);
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Location = new System.Drawing.Point(514, 190);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(120, 22);
+            this.textBoxC.TabIndex = 2;
+            this.textBoxC.Leave += new System.EventHandler(this.textBoxC_Leave);
+            // 
+            // numericUpDownA
+            // 
+            this.numericUpDownA.Location = new System.Drawing.Point(173, 229);
+            this.numericUpDownA.Name = "numericUpDownA";
+            this.numericUpDownA.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownA.TabIndex = 3;
+            this.numericUpDownA.ValueChanged += new System.EventHandler(this.numericUpDownA_ValueChanged);
+            // 
+            // numericUpDownB
+            // 
+            this.numericUpDownB.Location = new System.Drawing.Point(340, 229);
+            this.numericUpDownB.Name = "numericUpDownB";
+            this.numericUpDownB.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownB.TabIndex = 4;
+            this.numericUpDownB.ValueChanged += new System.EventHandler(this.numericUpDownB_ValueChanged);
+            // 
+            // numericUpDownC
+            // 
+            this.numericUpDownC.Location = new System.Drawing.Point(514, 229);
+            this.numericUpDownC.Name = "numericUpDownC";
+            this.numericUpDownC.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownC.TabIndex = 5;
+            this.numericUpDownC.ValueChanged += new System.EventHandler(this.numericUpDownC_ValueChanged);
+            // 
+            // trackBarA
+            // 
+            this.trackBarA.Location = new System.Drawing.Point(173, 271);
+            this.trackBarA.Maximum = 100;
+            this.trackBarA.Name = "trackBarA";
+            this.trackBarA.Size = new System.Drawing.Size(120, 56);
+            this.trackBarA.TabIndex = 6;
+            this.trackBarA.Scroll += new System.EventHandler(this.trackBarA_Scroll);
+            // 
+            // trackBarB
+            // 
+            this.trackBarB.Location = new System.Drawing.Point(340, 271);
+            this.trackBarB.Maximum = 100;
+            this.trackBarB.Name = "trackBarB";
+            this.trackBarB.Size = new System.Drawing.Size(120, 56);
+            this.trackBarB.TabIndex = 7;
+            this.trackBarB.Scroll += new System.EventHandler(this.trackBarB_Scroll);
+            // 
+            // trackBarC
+            // 
+            this.trackBarC.Location = new System.Drawing.Point(514, 271);
+            this.trackBarC.Maximum = 100;
+            this.trackBarC.Name = "trackBarC";
+            this.trackBarC.Size = new System.Drawing.Size(120, 56);
+            this.trackBarC.TabIndex = 8;
+            this.trackBarC.Scroll += new System.EventHandler(this.trackBarC_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(197, 88);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(70, 70);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(369, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 70);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(541, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 70);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "C";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(273, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 50);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "<=";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(451, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 50);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "<=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 467);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBarC);
+            this.Controls.Add(this.trackBarB);
+            this.Controls.Add(this.trackBarA);
+            this.Controls.Add(this.numericUpDownC);
+            this.Controls.Add(this.numericUpDownB);
+            this.Controls.Add(this.numericUpDownA);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarC)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.NumericUpDown numericUpDownA;
+        private System.Windows.Forms.NumericUpDown numericUpDownB;
+        private System.Windows.Forms.NumericUpDown numericUpDownC;
+        private System.Windows.Forms.TrackBar trackBarA;
+        private System.Windows.Forms.TrackBar trackBarB;
+        private System.Windows.Forms.TrackBar trackBarC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ laba4_2/laba4_2/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using laba4_2.Properties;
+
+// Данные, которые хранятся, логика, которая обрабатывает эти данные и их визуальное отображение должны быть разделены (храним все в разных местах)
+
+namespace laba4_2
+{
+    public partial class Form1 : Form
+    {
+        Model model; //Ссылка на Model        
+
+        public Form1()
+        {
+            InitializeComponent();
+            model = new Model(); //Форма создает модель
+            model.observer += new EventHandler(this.UpdateFromModel); //Форма подписывается на обновление модели
+                                                                      //При обновлении формы вызывается метод UodateFromModel
+        }
+
+        private void textBoxA_Leave(object sender, EventArgs e)
+        {
+            try // Для перехвата и обработки исключений
+            {
+                model.set_A_Value(Int32.Parse(textBoxA.Text)); //Преобразует строковое представление числа в эквивалентное ему 32-битовое целое число со знаком
+            }
+            catch (Exception) { }
+        }
+
+        private void textBoxB_Leave(object sender, EventArgs e)
+        {
+            try
+            {
+                model.set_B_Value(Int32.Parse(textBoxB.Text));
+            }
+            catch (Exception) { } //Exception - базовый для всех типов исключений
+        }
+
+        private void textBoxC_Leave(object sender, EventArgs e)
+        {
+            try
+            {
+                model.set_C_Value(Int32.Parse(textBoxC.Text));
+            }
+            catch (Exception) { }
+        }
+
+        private void numericUpDownA_ValueChanged(object sender, EventArgs e)
+        {
+            model.set_A_Value(Decimal.ToInt32(numericUpDownA.Value)); //Decimal представляет десятичное число с плавающей запятой
+        }
+
+        private void numericUpDownB_ValueChanged(object sender, EventArgs e)
+        {
+            model.set_B_Value(Decimal.ToInt32(numericUpDownB.Value));
+        }
+
+        private void numericUpDownC_ValueChanged(object sender, EventArgs e)
+        {
+            model.set_C_Value(Decimal.ToInt32(numericUpDownC.Value));
+        }
+
+        private void trackBarA_Scroll(object sender, EventArgs e)
+        {
+            model.set_A_Value(trackBarA.Value);
+        }
+
+        private void trackBarB_Scroll(object sender, EventArgs e)
+        {
+            model.set_B_Value(trackBarB.Value);
+        }
+
+        private void trackBarC_Scroll(object sender, EventArgs e)
+        {
+            model.set_C_Value(trackBarC.Value);
+        }
+        private void UpdateFromModel(object sender, EventArgs e)
+        {
+            //Когда модель обновляется, компоненты, отображаемые на форме, все разом получают обновлённое значение  
+             
+            textBoxA.Text = model.get_A_Value().ToString();
+            textBoxB.Text = model.get_B_Value().ToString();
+            textBoxC.Text = model.get_C_Value().ToString();
+
+            numericUpDownA.Value = model.get_A_Value();
+            numericUpDownB.Value = model.get_B_Value();
+            numericUpDownC.Value = model.get_C_Value();
+
+            trackBarA.Value = model.get_A_Value();
+            trackBarB.Value = model.get_B_Value();
+            trackBarC.Value = model.get_C_Value();
+        }        
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {            
+            model.saveValues();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            model.loadValues();
+        }
+
+
+        public class Model
+        {
+            private int valueA, valueB, valueC; //Место, где хранятся наши значения
+            public System.EventHandler observer;
+
+            //Описание всех бизнес-правил, которые позволяют менять значения одним образом и не позволяют другим
+            
+            public void set_A_Value(int value)
+            {
+                observer.Invoke(this, EventArgs.Empty);
+                if (value < 0) return;
+                if (value > 100) return;
+                if (value > valueB) set_B_Value(value);
+                if (value > valueC) set_C_Value(value);
+                valueA = value;
+                observer.Invoke(this, null);
+            }
+
+            public void set_B_Value(int value)
+            {
+                observer.Invoke(this, EventArgs.Empty);
+                if (value < 0) return;
+                if (value > 100) return;
+                if (value < valueA) return;
+                if (value > valueC) return;
+                valueB = value;
+                observer.Invoke(this, null); //Модель будет уведомлять всех тех, кто на нее подписался
+            }
+
+            public void set_C_Value(int value)
+            {
+                observer.Invoke(this, EventArgs.Empty);
+                if (value < 0) return;
+                if (value > 100) return;
+                if (value < valueA) set_A_Value(value);
+                if (value < valueB) set_B_Value(value);
+                valueC = value;
+                observer.Invoke(this, null);
+            }
+
+            public int get_A_Value()
+            {
+                return valueA;
+            }
+            public int get_B_Value()
+            {
+                return valueB;
+            }
+            public int get_C_Value()
+            {
+                return valueC;
+            }
+            public void saveValues()
+            {
+                Properties.Settings.Default.dataA = valueA;
+                Properties.Settings.Default.dataB = valueB;
+                Properties.Settings.Default.dataC = valueC;
+                Properties.Settings.Default.Save();
+            }
+            public void loadValues()
+            {
+                valueA = Properties.Settings.Default.dataA;
+                valueB = Properties.Settings.Default.dataB;
+                valueC = Properties.Settings.Default.dataC;
+                observer.Invoke(this, null);
+            }
+        }        
+    }
+}
