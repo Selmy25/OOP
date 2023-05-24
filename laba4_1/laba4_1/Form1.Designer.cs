387fec4753cdf11858ae565e48f32d62ed791c77
Selmy25 <emil.salmanov25@mail.ru>
2023-05-25 00:04:49 +0500

    First commit

---
+++ laba4_1/laba4_1/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace laba4_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBoxCtrl = new System.Windows.Forms.CheckBox();
+            this.checkBoxCrcl = new System.Windows.Forms.CheckBox();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkBoxCtrl
+            // 
+            this.checkBoxCtrl.AutoSize = true;
+            this.checkBoxCtrl.Location = new System.Drawing.Point(12, 12);
+            this.checkBoxCtrl.Name = "checkBoxCtrl";
+            this.checkBoxCtrl.Size = new System.Drawing.Size(91, 21);
+            this.checkBoxCtrl.TabIndex = 0;
+            this.checkBoxCtrl.Text = "Press Ctrl";
+            this.checkBoxCtrl.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxCrcl
+            // 
+            this.checkBoxCrcl.AutoSize = true;
+            this.checkBoxCrcl.Location = new System.Drawing.Point(12, 39);
+            this.checkBoxCrcl.Name = "checkBoxCrcl";
+            this.checkBoxCrcl.Size = new System.Drawing.Size(110, 21);
+            this.checkBoxCrcl.TabIndex = 1;
+            this.checkBoxCrcl.Text = "Many Circles";
+            this.checkBoxCrcl.UseVisualStyleBackColor = true;
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(12, 66);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(50, 32);
+            this.btnDel.TabIndex = 2;
+            this.btnDel.Text = "Del";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(860, 509);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.checkBoxCrcl);
+            this.Controls.Add(this.checkBoxCtrl);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBoxCtrl;
+        private System.Windows.Forms.CheckBox checkBoxCrcl;
+        private System.Windows.Forms.Button btnDel;
+    }
+}
+
---
+++ laba4_1/laba4_1/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*События KeyDown и KeyUp отправляются сфокусированному контролу. Если на форме есть другие контролы, и они сфокусированы, то они будут заглатывать эти события, и форме ничего не останется.
+  Поэтому, чтобы форме всегда приходили эти события, нужно установить для неё KeyPreview=true.
+*/
+
+namespace laba4_1
+{
+    public partial class Form1 : Form
+    {
+        private int radius = 0;
+        private List<CCircle> Circles = new List<CCircle>();
+        private int ctrl = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            radius = 25;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (CCircle Circle in Circles) // foreach перечисляет элементы коллекции и выполняет тело для каждого её элемента
+            {
+                Circle.drawCircle(e.Graphics);
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (ctrl == 0)
+            {
+                foreach (CCircle Circle1 in Circles)
+                {
+                    Circle1.setColor("Gray"); //Снятие выделения со всех объектов 
+                }
+                CCircle Circle = new CCircle(e.X, e.Y, radius);
+                Circles.Add(Circle);
+            }
+            if (ctrl == 1)
+            {
+                foreach (CCircle Circle1 in Circles)
+                {
+                    if (Circle1.mouseinCircle(e) == true)
+                    {
+                        checkBoxCrcl.Checked = true;
+                        break;
+                    }
+                }
+                Refresh(); // Refresh принудительно создает условия, при которых элемент управления делает недоступной свою клиентскую область и немедленно перерисовывает себя и все дочерние элементы
+            }
+            Refresh();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (ModifierKeys == Keys.Control)
+            {
+                checkBoxCtrl.Checked =! checkBoxCtrl.Checked;
+            }
+            switch (ctrl)
+            {
+                case 0:
+                    ctrl++;
+                    foreach (CCircle Circle1 in Circles)
+                    {
+                        Circle1.setCtrl(true);
+                    }
+                    break;
+                case 1:
+                    ctrl = 0;
+                    foreach (CCircle Circle1 in Circles)
+                    {
+                        Circle1.setCtrl(false);
+                    }
+                    break;
+            }
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Circles.Count(); i++)
+            {
+                if (Circles[i].getColor() == "Green")
+                {
+                    Circles.RemoveAt(i); // удаление элемента списка
+                    i--;
+                    checkBoxCrcl.Checked = false;
+                }
+            }
+            Refresh();
+        }
+
+        public class CCircle
+        {
+            private int coordX, coordY, radius;
+            private string color = "Green";
+            private bool ctrl = false;
+            public CCircle(int coordX, int coordY, int radius)
+            {
+                this.coordX = coordX;
+                this.coordY = coordY;
+                this.radius = radius;
+
+            }
+            public void drawCircle(Graphics Canvas)
+            {
+                if (color == "Green")
+                {
+                    Canvas.DrawEllipse(new Pen(Color.Green, radius * 2),  //Структура Pen, определяющая цвет, ширину и стиль эллипса.
+                                                       coordX - radius,   //Координата X верхнего левого угла ограничивающего прямоугольника, который определяет эллипс.
+                                                       coordY - radius,   //Координата Y верхнего левого угла ограничивающего прямоугольника, который определяет эллипс.
+                                                       radius * 2,        //Ширина ограничивающего прямоугольника, который определяет эллипс.
+                                                       radius * 2);       //Высота ограничивающего прямоугольника, который определяет эллипс.
+                }
+                else
+                {
+                    Canvas.DrawEllipse(new Pen(Color.Gray, radius * 2),
+                                                       coordX - radius,
+                                                       coordY - radius,
+                                                       radius * 2,
+                                                       radius * 2);
+                }
+            }
+            public void setColor(string Color)
+            {
+                color = Color;
+            }
+            public string getColor()
+            {
+                return color;
+            }
+            public bool mouseinCircle(MouseEventArgs e)
+            {
+                if (ctrl)
+                {
+                    if (Math.Pow(e.X - coordX, 2) + Math.Pow(e.Y - coordY, 2) <= Math.Pow(radius, 2) && color != "Green")
+                    {
+                        color = "Green";
+                        return true;
+                    }
+                }
+                return false;
+            }
+            public void setCtrl(bool a)
+            {
+                ctrl = a;
+            }
+
+        }
+    }
+}
